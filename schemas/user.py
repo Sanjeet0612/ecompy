7107a18e1ca48a65d12f4d2945c026334b262fd5
Sanjeet Yadav <sanjeetsinghyadav0612@gmail.com>
2026-06-24 17:24:06 +0530

    update

--- schemas/user.py
+++ schemas/user.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class UserCreate(BaseModel):
     name: str = Field(min_length=2, max_length=50)
     email: EmailStr
     phone: str | None = None
-    password: str = Field(min_length=6)
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        return value
