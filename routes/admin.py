70293070690c25acd1c3386ae91a7718a397d4b0
Sanjeet Yadav <sanjeetsinghyadav0612@gmail.com>
2026-06-29 21:47:42 +0530

    Admin Product list page and add product page created

--- routes/admin.py
+++ routes/admin.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 
 router = APIRouter(prefix="/admin")
 
@@ -20,11 +21,57 @@ def admin_login(request: Request):
 @router.get("/dashboard")
 def dashboard(request: Request):
 
-    admin = request.state.admin
+    admin = getattr(request.state, "admin", None)
+
+    if not admin:
+        return RedirectResponse(url="/admin/", status_code=302)
 
     return templates.TemplateResponse(
         request=request,
         name="admin/dashboard.html",
+        context={
+            "title": "Dashboard",
+            "breadcrumbs": [
+                {"name": "Admin", "url": None},
+                {"name": "Dashboard", "url": "/admin/dashboard"},
+            ],
+            "admin": admin
+        }
+    )
+
+@router.get("/products")
+def products(request: Request):
+
+    admin = getattr(request.state, "admin", None)
+
+    if not admin:
+        return RedirectResponse(url="/admin/", status_code=302)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="admin/manage_product.html",
+        context={
+            "title": "Dashboard",
+            "breadcrumbs": [
+                {"name": "Dashboard", "url": "/admin/dashboard"},
+                {"name": "Manage Products", "url": "/admin/products"},
+                {"name": "Add Product", "url": None}
+            ],
+            "admin": admin
+        }
+    )
+
+@router.get("/add-product")
+def add_products(request: Request):
+
+    admin = getattr(request.state, "admin", None)
+
+    if not admin:
+        return RedirectResponse(url="/admin/", status_code=302)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="admin/add_product.html",
         context={
             "title": "Dashboard",
             "admin": admin
